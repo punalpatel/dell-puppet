3e5de52a639c1a785507eb15941789c1af00f51c
Punal Patel <punal.patel@gmail.com>
2014-05-17 12:06:31 -0700

    first commit

---
+++ manifests/firmware.pp
@@ -0,0 +1,39 @@
+# == Class: dell::firmware
+#
+# Manage Dell system firmware packages
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#
+# === Variables
+#
+# [*sample_variable*]
+#
+# === Examples
+#
+#  class { 'dell::firmware': }
+#
+class dell::firmware(
+  $ensure = 'present',
+) inherits dell::params {
+
+  case $::osfamily {
+    'Debian' : {
+      package { 'firmware-addon-dell':
+        ensure  => $ensure,
+        require => Class['dell::repos'],
+      }
+      package { 'firmware-tools':
+        ensure  => $ensure,
+        require => Class['dell::repos'],
+      }
+    }
+    'RedHat' : {
+      package { 'dell_ft_install':
+        ensure  => $ensure,
+        require => Class['dell::repos'],
+      }
+    }
+  }
+}
---
+++ manifests/repos.pp
@@ -0,0 +1,101 @@
+# == Class: dell::repo
+#
+# This configures the dell package repositories
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#
+# === Variables
+#
+# [*sample_variable*]
+#
+# === Examples
+#
+#  class { 'dell::repos': }
+#
+class dell::repos() inherits dell::params {
+
+  case $::osfamily {
+    'Debian' : {
+
+      # Dell APT Repos
+      apt::key { 'dell-community':
+        key        => '34D8786F',
+        key_server => 'pool.sks-keyservers.net',
+      }
+
+      apt::source { 'dell-community':
+        location          => 'http://linux.dell.com/repo/community/ubuntu',
+        release           => '',
+        repos             => 'precise openmanage',
+        include_src       => false,
+      }
+
+    }
+    'RedHat' : {
+      ########################################
+      # Create the two repos
+      ########################################
+
+      # This would add duplicate repos, and we want to manage them explicitly
+      package { 'dell-omsa-repository':
+        ensure => 'absent',
+      }
+
+      yumrepo { 'dell-omsa-indep':
+        descr          => 'Dell OMSA repository - Hardware independent',
+        enabled        => 1,
+        mirrorlist     => $dell::params::repo_indep_mirrorlist,
+        gpgcheck       => 1,
+        gpgkey         => $dell::params::repo_indep_gpgkey,
+        failovermethod => 'priority',
+        require        => Package['dell-omsa-repository'],
+      } -> package { 'yum-dellsysid':  # I dislike this syntax, but require would not work for some reason...
+        ensure  => 'present',
+      }
+
+      yumrepo { 'dell-omsa-specific':
+        descr          => 'Dell OMSA repository - Hardware specific',
+        enabled        => 1,
+        mirrorlist     => $dell::params::repo_specific_mirrorlist,
+        gpgcheck       => 1,
+        gpgkey         => $dell::params::repo_specific_gpgkey,
+        failovermethod => 'priority',
+        require        => Package['dell-omsa-repository'],
+      }
+
+
+      ########################################
+      # GPG-KEY Management
+      ########################################
+      file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-dell':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/dell/RPM-GPG-KEY-dell',
+      }
+      exec { 'dell-RPM-GPG-KEY-dell':
+        command     => '/bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-dell',
+        subscribe   => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-dell'],
+        refreshonly => true,
+      }
+
+      file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios':
+        ensure => 'present',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => 'puppet:///modules/dell/RPM-GPG-KEY-libsmbios',
+      }
+      exec { 'dell-RPM-GPG-KEY-libsmbios':
+        command     => '/bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios',
+        subscribe   => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-libsmbios'],
+        refreshonly => true,
+      }
+
+    }
+  }
+}
+
